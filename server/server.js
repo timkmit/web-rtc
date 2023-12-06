a44dd7010caaef2cdec338e4d61a40674ed9a345
Timur <timengalytchev@mail.ru>
2023-12-07 01:25:19 +0300

    made: connect/disconnect sockets

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import ACTIONS from '../src/socket/actions';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
@@ -22,8 +23,72 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3088;
 
+function getClientRooms() {
+  const {rooms} = io.sockets.adapter;
+
+  return Array.from(rooms.keys())
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms()
+  })
+}
+
 io.on('connection', (socket) => {
   console.log('Socket connected');
+
+  socket.on(ACTIONS.JOIN, config => {
+    const {room: roomID} = config;
+    const {rooms: joinedRooms} = socket;
+
+    if(Array.from(joinedRooms).includes(roomID)){
+      return console.warn(`You are already at ${roomID} room`)
+    }
+
+    const client = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID : socket.id,
+        createOffer: false
+      }) //принимающая сторона добавляет офферы к пирам
+
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true
+      }) //будет создавать оффер сторона, которая подключается
+    })
+
+    socket.join(roomID);
+    shareRoomsInfo();
+  })
+
+  function leaveRoom() {
+    const {rooms} = socket;
+    Array.from(rooms)
+      .forEach(roomID => {
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients.forEach( clientID => {
+          io.to(clientID).emit(ACTIONS, {
+            peerID: socket.id,
+          }) //при выходе отправаляем айдишник остальным участникам
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          }) //себе при отключении отправляем айдишник клиента для отключения
+        })
+
+        socket.leave(roomID);
+      })
+
+      shareRoomsInfo();
+  }
+
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+  socket.on('disconnecting', leaveRoom);
+
 });
 
 server.listen(PORT, () => {
---
+++ src/socket/actions.js
@@ -0,0 +1,14 @@
+const ACTIONS = {
+    JOIN          : 'join',
+    LEAVE         : 'leave',
+    SHARE_ROOMS   : 'share-rooms',
+    REMOVE_PEER   : 'remove-peer',
+    ADD_PEER      : 'add-peer',
+    RELAY_SDP     : 'relay-sdp', //стримы с медиа данными
+    RELAY_ICE     : 'relay-ice', // физ подключения
+    ICE_CANDIDATE : 'ice-candidate', //реагируем
+    SESSION_DESCRIPTION : 'session-description',
+    
+}
+
+module.exports = ACTIONS;
