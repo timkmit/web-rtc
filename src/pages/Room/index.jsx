c76c14183f71b3a31e5adb508834b7913a74c978
Timur <timengalytchev@mail.ru>
2023-12-15 10:24:17 +0300

    just some magic from rrd :)

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,4 +1,12 @@
+import { useParams } from "react-router-dom";
+import useWebRTC from "../../hooks/useWebRTC";
+
 function Room() {
+    const {id: roomID} = useParams()
+
+    useWebRTC(roomID)
+
+    console.log(roomID)
     return ( 
         <div>
             Room page
